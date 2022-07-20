b910c9dcd6076a75c4ecb126d760abcec49e4a9f
Jason-R-Turner <jason.r.turner22@gmail.com>
2022-07-20 13:41:02 -0500

    catch up push

--- aquire.py
+++ aquire.py
@@ -1,1 +1,3 @@
-# Functions for CodeUp blog and news article aquisitions
+# Functions for CodeUp blog and news article aquisitions acquired from
+# acquired from acquire_codeup_blog and acquire_news_articles .py files
+
